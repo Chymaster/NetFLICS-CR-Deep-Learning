8560cd42596e3920180451f12cd7d5bec52c5255
Chymaster <xlin_cheng@outlook.com>
2022-08-17 16:51:48 +0100

    Progress on training_generator

--- training_data/training_generator.py
+++ training_data/training_generator.py
@@ -11,25 +11,50 @@ images_letters, _ = extract_training_samples('letters')
 images =np.concatenate((images_digits, images_letters))
 np.random.shuffle(images)
 
+## Genearate a lot of random ordered lifetime/intensity images
+
 # take 4 images:
 select_images = images[:4]
 
 # Padding
-select_images = utilities.padding(select_images)
+padded_images = np.zeros((select_images.shape[0],32,32))
+padded_images[:,2:-2,2:-2] = select_images
 
 
 # Turn into binary
-binary_selected_images = np.where(select_images > 0.5*np.max(select_images), 1, 0)
+binary_selected_images = np.where(padded_images > 0.5*np.max(padded_images), 1, 0)
 
+#############################################################################################################################################
+]# REPLACE, ROTATE AND FLIP IMAGE BEFORE SPLITTING THEM INTO INTENSITY AND LIFETIME IMAGE 
+#####################################################################################################################################################################
 # Generate intensity image
 intensity_images = binary_selected_images * np.random.uniform(low = 200, high = 800, size = (binary_selected_images.shape[0],1,1))		# Assign intensity values
 gaussian_intensity_images = np.zeros(intensity_images.shape)							# Gaussian Blur intensity images
 for i in range(len(intensity_images)):
 	gaussian_intensity_images[i,:,:] = gaussian_filter(intensity_images[i,:,:], 0.1)
 
-draw(gaussian_intensity_images)
-
 # Generate lifetime image
 lifetime = np.random.uniform(low = 0.3, high = 1.2)										# unit in ns
-lifetime_images = np.where(gaussian_intensity_images != 0, lifetime, 0) * np.random.uniform(low = 0.3, high = 1.2, size = (gaussian_intensity_images.shape[0],1,1))
-draw(lifetime_images)
+lifetime_images = np.where(gaussian_intensity_images != 0, 1, 0) * np.random.uniform(low = 0.3, high = 1.2, size = (gaussian_intensity_images.shape[0],1,1))
+#####################################################################################################################################################################
+
+
+## Combine into new image
+# Combine multiple
+column = np.random.randint(1,high = 5)
+row = np.random.randint(1,high = 5)
+number_of_characters = column*row
+pixel_per_character = 32
+
+# Order to combine characters
+order = np.array(range(number_of_characters-1))
+np.random.shuffle(order)
+
+# pick images to be combined
+pass
+# randomly flip/rotate every image
+pass
+# combine them into a single image
+
+combined_image = np.zeros((column*size_per_character, row*size_per_character))
+
